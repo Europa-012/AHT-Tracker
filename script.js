14b94407be2886b6eac84fc588f7441e3897f865
Gian Carlo Manuel <25800561+Europa-012@users.noreply.github.com>
2025-02-25 10:42:00 +0800

    Removed test cases

--- script.js
+++ script.js
@@ -244,7 +244,7 @@ window.onload = () => {
   // addTime();
   // newTimeInputBox.value = "05:00";
   // addTime();
-  let x = 20
+  let x = 0
   for (i = 0; i < x; i++) {
     newTimeInputBox.value = formatTime(String(Math.floor(Math.random() * 2000) + 1));
     addTime();
