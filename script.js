79c1bb98f0ec43f51aa9da45135932dd5c608975
Gian Carlo Manuel <25800561+Europa-012@users.noreply.github.com>
2025-03-10 03:37:49 +0800

    Fixed .main-input sizing issue. Added button display delay on hover for list items

--- script.js
+++ script.js
@@ -252,7 +252,7 @@ window.onload = () => {
   // addTime();
   // newTimeInputBox.value = "05:00";
   // addTime();
-  let x = 0
+  let x = 50
   for (i = 0; i < x; i++) {
     newTimeInputBox.value = formatTime(String(Math.floor(Math.random() * 2000) + 1));
     addTime();
